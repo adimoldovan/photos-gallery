4d7934cf8fee9b05224421c6e8fff0400fb558ef
Adrian Moldovan <adim.moldovan@gmail.com>
2021-10-11 21:14:09 +0300

    Counter.dev analytics script

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,7 @@
 <script>
   import Router from './routes/index.svelte';
+  // eslint-disable-next-line max-len
+  if( !sessionStorage.getItem( '_swa' )&&document.referrer.indexOf( location.protocol+'//'+location.host )!== 0 ) {fetch( 'https://counter.dev/track?'+new URLSearchParams( { referrer:document.referrer,screen:screen.width+'x'+screen.height,user:'adimoldovan',utcoffset:'3' } ) );}sessionStorage.setItem( '_swa','1' );
 </script>
 
 <main>
